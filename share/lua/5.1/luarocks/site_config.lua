68df967398716fba8eeb6fcf73d235adabacf40a
Thiago de Arruda <tpadilha84@gmail.com>
2014-08-29 21:29:49 -0300

    Fix 64-bit dependencies
    This also symlinks busted to busted_bootstrap to avoid a bug caused by luarocks
    update

--- share/lua/5.1/luarocks/site_config.lua
+++ share/lua/5.1/luarocks/site_config.lua
@@ -10,9 +10,9 @@ site_config.LUAROCKS_FORCE_CONFIG=true
 site_config.LUAROCKS_ROCKS_SUBDIR=[[/lib/luarocks/rocks]]
 site_config.LUA_DIR_SET=true
 site_config.LUAROCKS_UNAME_S=[[Linux]]
-site_config.LUAROCKS_UNAME_M=[[i686]]
+site_config.LUAROCKS_UNAME_M=[[x86_64]]
 site_config.LUAROCKS_DOWNLOADER=[[curl]]
 site_config.LUAROCKS_MD5CHECKER=[[md5sum]]
-site_config.LUAROCKS_EXTERNAL_DEPS_SUBDIRS={ bin="bin", lib={ "lib", [[lib/i386-linux-gnu]] }, include="include" }
-site_config.LUAROCKS_RUNTIME_EXTERNAL_DEPS_SUBDIRS={ bin="bin", lib={ "lib", [[lib/i386-linux-gnu]] }, include="include" }
+site_config.LUAROCKS_EXTERNAL_DEPS_SUBDIRS={ bin="bin", lib={ "lib", [[lib/x86_64-linux-gnu]] }, include="include" }
+site_config.LUAROCKS_RUNTIME_EXTERNAL_DEPS_SUBDIRS={ bin="bin", lib={ "lib", [[lib/x86_64-linux-gnu]] }, include="include" }
 return site_config
