8a7390280480bc7d2e9f0c577897d4a4ee1f6ee4
marvim <marvim@users.noreply.github.com>
2015-04-18 08:27:27 +0000

    deps64: Automatic update.

--- osx-x64/usr/share/lua/5.1/luassert/stub.lua
+++ osx-x64/usr/share/lua/5.1/luassert/stub.lua
@@ -2,18 +2,23 @@
 local assert = require 'luassert.assert'
 local spy = require 'luassert.spy'
 local util = require 'luassert.util'
-local stubfunc = function() end
 local stub = {}
+local unpack = require 'luassert.compatibility'.unpack
 
-function stub.new(object, key)
+function stub.new(object, key, ...)
   if object == nil and key == nil then
     -- called without arguments, create a 'blank' stub
     object = {}
     key = ""
   end
+  local return_values_count = select("#", ...)
+  local return_values = {...}
   assert(type(object) == "table" and key ~= nil, "stub.new(): Can only create stub on a table key, call with 2 params; table, key")
   assert(object[key] == nil or util.callable(object[key]), "stub.new(): The element for which to create a stub must either be callable, or be nil")
   local old_elem = object[key]    -- keep existing element (might be nil!)
+  local stubfunc = function()
+    return unpack(return_values, 1, return_values_count)
+  end
   object[key] = stubfunc          -- set the stubfunction
   local s = spy.on(object, key)   -- create a spy on top of the stub function
   local spy_revert = s.revert     -- keep created revert function
@@ -28,10 +33,6 @@ function stub.new(object, key)
   end
   
   return s
-end
-
-function stub.is_stub(object)
-  return spy.is_spy(object) and object.callback == stubfunc
 end
 
 local function set_stub(state)
