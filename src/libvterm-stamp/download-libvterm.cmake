cbebb81009ff4baec45b6542d9f22be65ffc1b01
marvim <marvim@users.noreply.github.com>
2023-10-30 05:16:10 +0000

    deps src: Automatic update

--- src/libvterm-stamp/download-libvterm.cmake
+++ src/libvterm-stamp/download-libvterm.cmake
@@ -22,16 +22,16 @@ function(check_file_hash has_hash hash_is_good)
   set("${has_hash}" TRUE PARENT_SCOPE)
 
   message(STATUS "verifying file...
-       file='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz'")
+       file='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz'")
 
-  file("SHA256" "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz" actual_value)
+  file("SHA256" "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz" actual_value)
 
-  if(NOT "${actual_value}" STREQUAL "09156f43dd2128bd347cbeebe50d9a571d32c64e0cf18d211197946aff7226e0")
+  if(NOT "${actual_value}" STREQUAL "0babe3ab42c354925dadede90d352f054aa9c4ae6842ea803a20c9741e172e56")
     set("${hash_is_good}" FALSE PARENT_SCOPE)
     message(STATUS "SHA256 hash of
-    /home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz
+    /home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz
   does not match expected value
-    expected: '09156f43dd2128bd347cbeebe50d9a571d32c64e0cf18d211197946aff7226e0'
+    expected: '0babe3ab42c354925dadede90d352f054aa9c4ae6842ea803a20c9741e172e56'
       actual: '${actual_value}'")
   else()
     set("${hash_is_good}" TRUE PARENT_SCOPE)
@@ -71,40 +71,40 @@ function(sleep_before_download attempt)
   execute_process(COMMAND "${CMAKE_COMMAND}" -E sleep "${sleep_seconds}")
 endfunction()
 
-if("/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz" STREQUAL "")
+if("/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz" STREQUAL "")
   message(FATAL_ERROR "LOCAL can't be empty")
 endif()
 
-if("https://github.com/neovim/deps/raw/12c9dcf1d823ac4acbccf494c93c4774a87db11d/opt/libvterm-0.3.3.tar.gz" STREQUAL "")
+if("https://github.com/neovim/libvterm/archive/v0.3.3.tar.gz" STREQUAL "")
   message(FATAL_ERROR "REMOTE can't be empty")
 endif()
 
-if(EXISTS "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz")
+if(EXISTS "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz")
   check_file_hash(has_hash hash_is_good)
   if(has_hash)
     if(hash_is_good)
       message(STATUS "File already exists and hash match (skip download):
-  file='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz'
-  SHA256='09156f43dd2128bd347cbeebe50d9a571d32c64e0cf18d211197946aff7226e0'"
+  file='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz'
+  SHA256='0babe3ab42c354925dadede90d352f054aa9c4ae6842ea803a20c9741e172e56'"
       )
       return()
     else()
       message(STATUS "File already exists but hash mismatch. Removing...")
-      file(REMOVE "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz")
+      file(REMOVE "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz")
     endif()
   else()
     message(STATUS "File already exists but no hash specified (use URL_HASH):
-  file='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz'
+  file='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz'
 Old file will be removed and new file downloaded from URL."
     )
-    file(REMOVE "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz")
+    file(REMOVE "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz")
   endif()
 endif()
 
 set(retry_number 5)
 
 message(STATUS "Downloading...
-   dst='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz'
+   dst='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz'
    timeout='none'
    inactivity timeout='none'"
 )
@@ -115,7 +115,7 @@ foreach(i RANGE ${retry_number})
   if(status_code IN_LIST download_retry_codes)
     sleep_before_download(${i})
   endif()
-  foreach(url https://github.com/neovim/deps/raw/12c9dcf1d823ac4acbccf494c93c4774a87db11d/opt/libvterm-0.3.3.tar.gz)
+  foreach(url https://github.com/neovim/libvterm/archive/v0.3.3.tar.gz)
     if(NOT url IN_LIST skip_url_list)
       message(STATUS "Using src='${url}'")
 
@@ -126,7 +126,7 @@ foreach(i RANGE ${retry_number})
 
       file(
         DOWNLOAD
-        "${url}" "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz"
+        "${url}" "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz"
         
         # no TIMEOUT
         # no INACTIVITY_TIMEOUT
@@ -143,7 +143,7 @@ foreach(i RANGE ${retry_number})
         check_file_hash(has_hash hash_is_good)
         if(has_hash AND NOT hash_is_good)
           message(STATUS "Hash mismatch, removing...")
-          file(REMOVE "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/libvterm-0.3.3.tar.gz")
+          file(REMOVE "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/v0.3.3.tar.gz")
         else()
           message(STATUS "Downloading... done")
           return()
