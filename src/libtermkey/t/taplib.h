d6245ab7fa3ef25b006b89d61bcbe055a0986c9b
marvim <marvim@users.noreply.github.com>
2023-12-01 05:16:12 +0000

    deps src: Automatic update

--- src/libtermkey/t/taplib.h
+++
@@ -1,7 +0,0 @@
-void plan_tests(int n);
-void ok(int cmp, char *name);
-void pass(char *name);
-void fail(char *name);
-void is_int(int got, int expect, char *name);
-void is_str(const char *got, const char *expect, char *name);
-int exit_status(void);
