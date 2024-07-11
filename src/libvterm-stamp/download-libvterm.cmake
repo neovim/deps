4d1a1cd8bcf3c18acd89a7bf95437cd02f9893cb
marvim <marvim@users.noreply.github.com>
2024-07-11 05:18:19 +0000

    deps src: Automatic update

--- src/libvterm-stamp/download-libvterm.cmake
+++ src/libvterm-stamp/download-libvterm.cmake
@@ -21,14 +21,14 @@ function(check_file_hash has_hash hash_is_good)
 
   set("${has_hash}" TRUE PARENT_SCOPE)
 
-  message(STATUS "verifying file...
+  message(VERBOSE "verifying file...
        file='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/0a15c6e983b0db7ef8276e0792414a805d01bdaf.tar.gz'")
 
   file("SHA256" "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/0a15c6e983b0db7ef8276e0792414a805d01bdaf.tar.gz" actual_value)
 
   if(NOT "${actual_value}" STREQUAL "c4683e7a2d71c04781fd0ab7719a94202800e97a9e091514c16983bb732b0fa7")
     set("${hash_is_good}" FALSE PARENT_SCOPE)
-    message(STATUS "SHA256 hash of
+    message(VERBOSE "SHA256 hash of
     /home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/0a15c6e983b0db7ef8276e0792414a805d01bdaf.tar.gz
   does not match expected value
     expected: 'c4683e7a2d71c04781fd0ab7719a94202800e97a9e091514c16983bb732b0fa7'
@@ -44,7 +44,7 @@ function(sleep_before_download attempt)
   endif()
 
   if(attempt EQUAL 1)
-    message(STATUS "Retrying...")
+    message(VERBOSE "Retrying...")
     return()
   endif()
 
@@ -66,7 +66,7 @@ function(sleep_before_download attempt)
     set(sleep_seconds 1200)
   endif()
 
-  message(STATUS "Retry after ${sleep_seconds} seconds (attempt #${attempt}) ...")
+  message(VERBOSE "Retry after ${sleep_seconds} seconds (attempt #${attempt}) ...")
 
   execute_process(COMMAND "${CMAKE_COMMAND}" -E sleep "${sleep_seconds}")
 endfunction()
@@ -75,17 +75,17 @@ if(EXISTS "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/0a15
   check_file_hash(has_hash hash_is_good)
   if(has_hash)
     if(hash_is_good)
-      message(STATUS "File already exists and hash match (skip download):
+      message(VERBOSE "File already exists and hash match (skip download):
   file='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/0a15c6e983b0db7ef8276e0792414a805d01bdaf.tar.gz'
   SHA256='c4683e7a2d71c04781fd0ab7719a94202800e97a9e091514c16983bb732b0fa7'"
       )
       return()
     else()
-      message(STATUS "File already exists but hash mismatch. Removing...")
+      message(VERBOSE "File already exists but hash mismatch. Removing...")
       file(REMOVE "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/0a15c6e983b0db7ef8276e0792414a805d01bdaf.tar.gz")
     endif()
   else()
-    message(STATUS "File already exists but no hash specified (use URL_HASH):
+    message(VERBOSE "File already exists but no hash specified (use URL_HASH):
   file='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/0a15c6e983b0db7ef8276e0792414a805d01bdaf.tar.gz'
 Old file will be removed and new file downloaded from URL."
     )
@@ -95,12 +95,12 @@ endif()
 
 set(retry_number 5)
 
-message(STATUS "Downloading...
+message(VERBOSE "Downloading...
    dst='/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/0a15c6e983b0db7ef8276e0792414a805d01bdaf.tar.gz'
    timeout='none'
    inactivity timeout='none'"
 )
-set(download_retry_codes 7 6 8 15 28)
+set(download_retry_codes 7 6 8 15 28 35)
 set(skip_url_list)
 set(status_code)
 foreach(i RANGE ${retry_number})
@@ -109,8 +109,9 @@ foreach(i RANGE ${retry_number})
   endif()
   foreach(url IN ITEMS [====[https://github.com/neovim/libvterm/archive/0a15c6e983b0db7ef8276e0792414a805d01bdaf.tar.gz]====])
     if(NOT url IN_LIST skip_url_list)
-      message(STATUS "Using src='${url}'")
+      message(VERBOSE "Using src='${url}'")
 
+      
       
       
       
@@ -134,10 +135,10 @@ foreach(i RANGE ${retry_number})
       if(status_code EQUAL 0)
         check_file_hash(has_hash hash_is_good)
         if(has_hash AND NOT hash_is_good)
-          message(STATUS "Hash mismatch, removing...")
+          message(VERBOSE "Hash mismatch, removing...")
           file(REMOVE "/home/runner/work/deps/deps/neovim/deps/build/downloads/libvterm/0a15c6e983b0db7ef8276e0792414a805d01bdaf.tar.gz")
         else()
-          message(STATUS "Downloading... done")
+          message(VERBOSE "Downloading... done")
           return()
         endif()
       else()
--- src/utf8proc-stamp/download-utf8proc.cmake
+++ src/utf8proc-stamp/download-utf8proc.cmake
@@ -21,14 +21,14 @@ function(check_file_hash has_hash hash_is_good)
 
   set("${has_hash}" TRUE PARENT_SCOPE)
 
-  message(STATUS "verifying file...
+  message(VERBOSE "verifying file...
        file='/home/runner/work/deps/deps/neovim/deps/build/downloads/utf8proc/v2.9.0.tar.gz'")
 
   file("SHA256" "/home/runner/work/deps/deps/neovim/deps/build/downloads/utf8proc/v2.9.0.tar.gz" actual_value)
 
   if(NOT "${actual_value}" STREQUAL "18c1626e9fc5a2e192311e36b3010bfc698078f692888940f1fa150547abb0c1")
     set("${hash_is_good}" FALSE PARENT_SCOPE)
-    message(STATUS "SHA256 hash of
+    message(VERBOSE "SHA256 hash of
     /home/runner/work/deps/deps/neovim/deps/build/downloads/utf8proc/v2.9.0.tar.gz
   does not match expected value
     expected: '18c1626e9fc5a2e192311e36b3010bfc698078f692888940f1fa150547abb0c1'
@@ -44,7 +44,7 @@ function(sleep_before_download attempt)
   endif()
 
   if(attempt EQUAL 1)
-    message(STATUS "Retrying...")
+    message(VERBOSE "Retrying...")
     return()
   endif()
 
@@ -66,7 +66,7 @@ function(sleep_before_download attempt)
     set(sleep_seconds 1200)
   endif()
 
-  message(STATUS "Retry after ${sleep_seconds} seconds (attempt #${attempt}) ...")
+  message(VERBOSE "Retry after ${sleep_seconds} seconds (attempt #${attempt}) ...")
 
   execute_process(COMMAND "${CMAKE_COMMAND}" -E sleep "${sleep_seconds}")
 endfunction()
@@ -75,17 +75,17 @@ if(EXISTS "/home/runner/work/deps/deps/neovim/deps/build/downloads/utf8proc/v2.9
   check_file_hash(has_hash hash_is_good)
   if(has_hash)
     if(hash_is_good)
-      message(STATUS "File already exists and hash match (skip download):
+      message(VERBOSE "File already exists and hash match (skip download):
   file='/home/runner/work/deps/deps/neovim/deps/build/downloads/utf8proc/v2.9.0.tar.gz'
   SHA256='18c1626e9fc5a2e192311e36b3010bfc698078f692888940f1fa150547abb0c1'"
       )
       return()
     else()
-      message(STATUS "File already exists but hash mismatch. Removing...")
+      message(VERBOSE "File already exists but hash mismatch. Removing...")
       file(REMOVE "/home/runner/work/deps/deps/neovim/deps/build/downloads/utf8proc/v2.9.0.tar.gz")
     endif()
   else()
-    message(STATUS "File already exists but no hash specified (use URL_HASH):
+    message(VERBOSE "File already exists but no hash specified (use URL_HASH):
   file='/home/runner/work/deps/deps/neovim/deps/build/downloads/utf8proc/v2.9.0.tar.gz'
 Old file will be removed and new file downloaded from URL."
     )
@@ -95,12 +95,12 @@ endif()
 
 set(retry_number 5)
 
-message(STATUS "Downloading...
+message(VERBOSE "Downloading...
    dst='/home/runner/work/deps/deps/neovim/deps/build/downloads/utf8proc/v2.9.0.tar.gz'
    timeout='none'
    inactivity timeout='none'"
 )
-set(download_retry_codes 7 6 8 15 28)
+set(download_retry_codes 7 6 8 15 28 35)
 set(skip_url_list)
 set(status_code)
 foreach(i RANGE ${retry_number})
@@ -109,8 +109,9 @@ foreach(i RANGE ${retry_number})
   endif()
   foreach(url IN ITEMS [====[https://github.com/JuliaStrings/utf8proc/archive/v2.9.0.tar.gz]====])
     if(NOT url IN_LIST skip_url_list)
-      message(STATUS "Using src='${url}'")
+      message(VERBOSE "Using src='${url}'")
 
+      
       
       
       
@@ -134,10 +135,10 @@ foreach(i RANGE ${retry_number})
       if(status_code EQUAL 0)
         check_file_hash(has_hash hash_is_good)
         if(has_hash AND NOT hash_is_good)
-          message(STATUS "Hash mismatch, removing...")
+          message(VERBOSE "Hash mismatch, removing...")
           file(REMOVE "/home/runner/work/deps/deps/neovim/deps/build/downloads/utf8proc/v2.9.0.tar.gz")
         else()
-          message(STATUS "Downloading... done")
+          message(VERBOSE "Downloading... done")
           return()
         endif()
       else()
