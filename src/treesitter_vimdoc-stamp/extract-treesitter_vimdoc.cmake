0ac00e7ab390ba25c7572426a4dea0cb0de28fe8
marvim <marvim@users.noreply.github.com>
2025-12-16 05:33:19 +0000

    deps src: Automatic update

--- src/treesitter_vimdoc-stamp/extract-treesitter_vimdoc.cmake
+++ src/treesitter_vimdoc-stamp/extract-treesitter_vimdoc.cmake
@@ -5,7 +5,7 @@ cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
 
 # Make file names absolute:
 #
-get_filename_component(filename "/home/runner/work/deps/deps/neovim/deps/build/downloads/treesitter_vimdoc/v4.0.2.tar.gz" ABSOLUTE)
+get_filename_component(filename "/home/runner/work/deps/deps/neovim/deps/build/downloads/treesitter_vimdoc/v4.1.0.tar.gz" ABSOLUTE)
 get_filename_component(directory "/home/runner/work/deps/deps/neovim/deps/build/src/treesitter_vimdoc" ABSOLUTE)
 
 message(VERBOSE "extracting...
