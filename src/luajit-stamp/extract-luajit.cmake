aed77c054f98c22f9be7f5eedbb3bb11bc02e19e
marvim <marvim@users.noreply.github.com>
2026-07-19 07:32:42 +0000

    deps src: Automatic update

--- src/luajit-stamp/extract-luajit.cmake
+++ src/luajit-stamp/extract-luajit.cmake
@@ -5,7 +5,7 @@ cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
 
 # Make file names absolute:
 #
-get_filename_component(filename "/home/runner/work/deps/deps/neovim/deps/build/downloads/luajit/3c4f9fe2052b8d08a917ac0d5f38563f0297b5a3.tar.gz" ABSOLUTE)
+get_filename_component(filename "/home/runner/work/deps/deps/neovim/deps/build/downloads/luajit/14d8a7a27dc8c626ab9e7c7e9e50b6df6def4f03.tar.gz" ABSOLUTE)
 get_filename_component(directory "/home/runner/work/deps/deps/neovim/deps/build/src/luajit" ABSOLUTE)
 
 message(VERBOSE "extracting...
